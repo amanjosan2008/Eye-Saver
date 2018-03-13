b4d564f587361cda28b50d5486eb73be72bc21d5
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-03-13 20:55:09 +0530

    Update and rename crontab.sh to install.sh

--- crontab.sh
+++
@@ -1,7 +0,0 @@
-# This script will install a Crontab entry to 
-# execute eyesaver warning every 20 minutes
-
-echo "*/20 * * * * /usr/bin/python ~/eyesaver.sh" | sudo tee --append /etc/cron.d/eyesaver > /dev/null
-mv eye-saver.py ~
-
-
---
+++ install.sh
@@ -0,0 +1,11 @@
+# This script will install a Crontab entry 
+# to run script every 20 minutes
+# Also it will copy the script to home directory
+
+
+mv eye-saver.py ~
+echo "*/20 * * * * /usr/bin/python ~/eyesaver.sh" | sudo tee --append /etc/cron.d/eyesaver > /dev/null
+crontab eyesaver 
+
+
+
