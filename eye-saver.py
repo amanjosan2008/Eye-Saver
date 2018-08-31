077a6583b100d0763b9bd0c10d5156ccff7cb4d8
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-08-31 10:09:47 +0530

    Update eye-saver.py

--- eye-saver.py
+++ eye-saver.py
@@ -1,47 +1,39 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 try:
     from tkinter import *
 except:
     from Tkinter import *
-import sys
-#import webbrowser
-#import random
-#import time
+import time
+import os
+
+text="Look at an Object\n 20 Feets away\n For 20 seconds"
+
+Time = time.strftime("%I:%M %p", time.localtime())
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+log_file = os.path.join(dir_path, 'eye-saver.log')
+f = open(log_file,'a')
+f.write(Time + ': ' + 'Eye Saver script executed\n')
+f.close()
+
+def display():
+    l.config(text=text, fg="black")
+    root.after(100, display)
 
 root = Tk()
-text="Look at an Object\n 20 feet away\n for 20 seconds"
-
-#videos = ('https://www.youtube.com/watch?v=GSO6g3dNR7s',
-#'https://www.youtube.com/watch?v=Y2dHYfb5OnE',
-#'https://www.youtube.com/watch?v=uiKg6JfS658',
-#'https://www.youtube.com/watch?v=azCzW_0GADM')
-
-#webbrowser.open(random.choice(videos))
-
-def center(toplevel):
-    toplevel.update_idletasks()
-    w = toplevel.winfo_screenwidth()
-    h = toplevel.winfo_screenheight()
-    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-    x = w/2 - size[0]/2
-    y = h/2 - size[1]/2
-    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
-
-
-frame = Frame(root, bd=1, relief=GROOVE)
-frame.grid()
-#video.attach_window(frame.window_id())
-
-Label(frame, text=text, fg="red", font=("Helvetica", 24), anchor=CENTER, justify=CENTER).grid()
-#Button(frame, text="Close", command=exit).grid(row=1, column=1)
-
-center(root)
-root.title("Eye Saver: Tool to Saver Vision")
-#root.attributes("-fullscreen", True)
-root.bind('<Escape>', exit)
+root.wm_overrideredirect(True)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.after(20000, lambda: root.destroy())
+root.lift()
+root.call('wm', 'attributes', '.', '-topmost', '1')
+#root.bind('<Escape>', exit)
+
+l = Label(text='', font=("Helvetica", 60))
+l.pack(expand=True)
+
+b = Button(root, text="Close", command=exit)
+b.pack(anchor='se')
+
+display()
 root.mainloop()
-
-
-
